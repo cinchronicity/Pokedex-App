c081bb5ed3c7b9155d2bed8395cf6247a597774b
cinchronicity <168139730+cinchronicity@users.noreply.github.com>
2024-07-19 16:39:59 -0700

    add  pokemon list array and properties

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
-alert('Hello World');
-let favoriteFood= 'thai';
-document.write(favoriteFood);
+//obj is in the curly braces, and the key value pairs are separated by commas, the keys are strings  (name, height, type), and the values (which are the preoperties of the pokemon) can be any data type (string, number, boolean, array, object, function)
+let pokemonList = [
+    { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+    { name: "Ivysaur", height: 1, type: ["grass", "poison"] },
+    { name: "Venusaur", height: 2, type: ["grass", "poison"] },
+  ];
+  
