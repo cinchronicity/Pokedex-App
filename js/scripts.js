183ff45b2c0b021d2c4e3143b9dbd0f7140f5386
cinchronicity <168139730+cinchronicity@users.noreply.github.com>
2024-07-21 22:40:05 -0700

    added for loop with conditional for pokemonList

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,14 @@
 //obj is in the curly braces, and the key value pairs are separated by commas, the keys are strings  (name, height, type), and the values (which are the preoperties of the pokemon) can be any data type (string, number, boolean, array, object, function)
 let pokemonList = [
-    { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
-    { name: "Ivysaur", height: 1, type: ["grass", "poison"] },
-    { name: "Venusaur", height: 2, type: ["grass", "poison"] },
-  ];
-  
+  { name: "Bulbasaur", height: 0.7, type: ["grass", "poison"] },
+  { name: "Ivysaur", height: 1, type: ["grass", "poison"] },
+  { name: "Venusaur", height: 2, type: ["grass", "poison"] },
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+  let pokemon = pokemonList[i];
+  document.write("<p>" + pokemon.name + " (height: " + pokemon.height + ")");
+  if (pokemon.height > 1.0) {
+    document.write("  - Wow, that’s big!");
+  }
+}
