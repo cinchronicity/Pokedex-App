bcb1c8fc4192ee0a9b8be96c66593ce8c4ab7a1c
cinchronicity <168139730+cinchronicity@users.noreply.github.com>
2024-08-15 15:14:03 -0700

    ESC button functionality for modal

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ let pokemonRepository = (function () {
   //IIFE
   let pokemonList = []; // empty array of objects
   let apiUrl = "https://pokeapi.co/api/v2/pokemon/?limit=150"; //url to fetch the pokemon data
-  let modalContainer = document.querySelector("#modal-container");
+  let modalContainer = document.querySelector(".modal");
 
   function add(pokemon) {
     //function to add a new pokemon to the pokemonList array
@@ -25,7 +25,7 @@ let pokemonRepository = (function () {
     return pokemonList; //return the array
   }
 
-  // function filterName(name) { 
+  // function filterName(name) {
   //   return pokemonList.filter(
   //     (pokemon) => pokemon.name.toLowerCase() === name.toLowerCase()
   //   );
@@ -56,11 +56,10 @@ let pokemonRepository = (function () {
           let pokemon = {
             //create a pokemon object with the name and detailsUrl properties
             name: item.name,
-            type: item.types, 
+            type: item.types,
             detailsUrl: item.url,
           };
           add(pokemon); //call the add function with the pokemon object as an argument
-          
         });
       })
       .catch(function (e) {
@@ -79,7 +78,7 @@ let pokemonRepository = (function () {
         // Now we add the details to the item
         item.imageUrl = details.sprites.front_default;
         item.height = details.height;
-        //item.type = details.types; // modify typoes w a for loop to iterate thru the object and push the type u want into an empty array of types to the userrs 
+        //item.type = details.types; // modify typoes w a for loop to iterate thru the object and push the type u want into an empty array of types to the userrs
         item.types = details.types.map((type) => type.type.name); //extract the type name from the type object in the details.types array, and store it in the item.types array. map() method creates a new array with thhe results of details.types array and the new array assigned to item.types????
       })
       .catch(function (e) {
@@ -87,49 +86,69 @@ let pokemonRepository = (function () {
       });
   }
 
-
   function showDetails(item) {
     //uses the pokemon repo to log the details of the pokemon to the console
     pokemonRepository.loadDetails(item).then(function () {
       //loadDetails fn called w item as an argument
-      pokemonRepository.showModal(item.name, item.types, item.height, item.imageUrl); //showModal fn called w item.name and item.height as arguments to display the pokemon name and height
-    }); 
+      pokemonRepository.showModal(
+        item.name,
+        item.types,
+        item.height,
+        item.imageUrl
+      ); //showModal fn called w item.name and item.height as arguments to display the pokemon name and height
+    });
   }
- 
 
   function addListener(button, pokemon) {
     //function to add an event listener to the button
     button.addEventListener("click", () => showDetails(pokemon)); //when clicked, showDetails fn called w pokemon as obj.
   }
 
-  
-
   function showModal(name, type, height, imageUrl) {
-    let modal = document.getElementById('pokemonModal');
-    let nameElement = document.getElementById('pokemonName'); 
-    let typeElement = document.getElementById('pokemonTypes');
-    let heightElement = document.getElementById('pokemonHeight');
-    let imageElement = document.getElementById('pokemonImage');
-    let closeButton = document.querySelector('.close');
-  
-    nameElement.innerText = `Name: ${name}`;
+    let modal = document.getElementById("pokemonModal");
+    let nameElement = document.getElementById("pokemonName");
+    let typeElement = document.getElementById("pokemonTypes");
+    let heightElement = document.getElementById("pokemonHeight");
+    let imageElement = document.getElementById("pokemonImage");
+    let closeButton = document.querySelector(".close");
+
+    nameElement.innerText = `Name: ${name}`; //innerText property sets or returns the text content of the specified node, and all its descendants.
     typeElement.innerText = `Type: ${type}`;
     heightElement.innerText = `Height: ${height}`;
-    imageElement.src = imageUrl; 
-  
-    modal.style.display = 'block';
-  
-    closeButton.onclick = function() {
-      modal.style.display = 'none';
+    imageElement.src = imageUrl;
+
+    modal.style.display = "block";
+    modalContainer.classList.add("is-visible");
+
+    closeButton.onclick = function () {
+      hideModal();
+      // modal.style.display = 'none'; //when the user clicks on the close button, close the modal
     };
-  
-    window.onclick = function(event) {
+
+    window.onclick = function (event) {
+      //when the user clicks anywhere outside of the modal, close it
       if (event.target == modal) {
-        modal.style.display = 'none';
+        hideModal();
+        // modal.style.display = 'none';
       }
     };
+    window.addEventListener("keydown", (e) => {
+      if (
+        e.key === "Escape" &&
+        modalContainer.classList.contains("is-visible")
+      ) {
+        hideModal();
+      }
+    });
   }
-  
+
+  function hideModal() {
+    let modal = document.getElementById("pokemonModal");
+    if (modalContainer.classList.contains("is-visible")) {
+      modal.style.display = "none";
+      modalContainer.classList.remove("is-visible");
+    }
+  }
 
   return {
     //new object has getAll and add-return an object with two keys, add and getAll
@@ -138,9 +157,9 @@ let pokemonRepository = (function () {
     // filterName: filterName, will use later for search bar functionality
     addListItem: addListItem,
     loadList: loadList,
-    loadDetails: loadDetails, 
-    showDetails: showDetails, 
-    showModal: showModal, 
+    loadDetails: loadDetails,
+    showDetails: showDetails,
+    showModal: showModal,
   }; //end of return
 })(); //end of IIFE
 
